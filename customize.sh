4fd9335d8e2360455e40f66ce737e57c6cbe8470
854675824 <854675824@qq.com>
2024-10-07 01:06:12 +0800

    更新 All-In-One v1.3-fix 版本

--- customize.sh
+++ customize.sh
@@ -3,8 +3,8 @@ ui_print "- 正在释放文件"
 # unzip -o "$ZIPFILE" 'RESOURCES/system/*' -d $MODPATH >&2
 # 复制配置文件到 /storage/emulated/0/Android/config.yaml 路径
 unzip -o "$ZIPFILE" 'config.yaml' -d "/storage/emulated/0/Android/" >&2
-echo "[$(date '+%m-%d %H:%M:%S.%3N')] All-In-One v1.2 模块安装成功, 等待重启" >> "/storage/emulated/0/Android/config.yaml.log"
-ui_print "- All-In-One 优化模块 v1.3"
+echo "[$(date '+%m-%d %H:%M:%S.%3N')] All-In-One v1.3-fix 修复版本模块安装成功, 等待重启" >> "/storage/emulated/0/Android/config.yaml.log"
+ui_print "- All-In-One 优化模块 v1.3-fix 修复版本"
 ui_print "- 对系统 CPU / GPU / 内存深度优化"
 ui_print "- 模块可以释放手机的全部性能"
 ui_print "- 可自行查看 service.sh 与 system.prop 文件"
